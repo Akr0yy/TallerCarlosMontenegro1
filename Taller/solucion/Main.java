9f2ce541b807ba2f196d2ddc2685f89577e58e77
Akr0yy <carlos.a.montenegro.p@gmail.com>
2026-03-28 14:34:14 -0300

    Primer commit

--- Taller/solucion/Main.java
+++ Taller/solucion/Main.java
@@ -1,9 +1,47 @@
 package solucion;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException{
+		
+		File arch = new File("Usuarios.txt");
+		Scanner lector = new Scanner(arch);
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("1) Menu de usuarios");
+		System.out.println("2) Menu de analisis");
+		System.out.println("3) Salir");
+		System.out.print("> ");
+		
+		int opcion = s.nextInt();
+		
+		switch (opcion) {
+		case 1:
+			System.out.print("Usuario: ");
+			String usuario = s.next();
+			System.out.print("Contraseña: ");
+			String contraseña = s.next();
+			
+
+		}
+		
+				
+		
+		while (lector.hasNextLine()) {
+			String linea = lector.nextLine();
+			String[] partes = linea.split(";");
+			
+			String usuario = partes [0];
+			String contraseña = partes[1];
+			System.out.printf("Tu usuario es %s, y tu contraseña es %s \n", usuario, contraseña);
+			//
+		}
+		
+		lector.close();
 
 	}
 
